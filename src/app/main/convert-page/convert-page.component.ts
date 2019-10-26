82893db571058f08c8835b62918f06cd917b2469
Sergey <mazilanx@gmail.com>
2019-10-26 16:02:29 +0300

    added notifi message

--- src/app/main/convert-page/convert-page.component.ts
+++ src/app/main/convert-page/convert-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {NotifyService} from "../../core/notify.service";
 
 @Component({
   selector: 'app-convert-page',
@@ -10,8 +11,11 @@ export class ConvertPageComponent implements OnInit {
     selected = 'USD';
 
     constructor(
+        private notify: NotifyService,
     ) {}
 
     ngOnInit() {
+        //use for test notifi
+        // this.notify.update('test', 'error');
     }
 }
