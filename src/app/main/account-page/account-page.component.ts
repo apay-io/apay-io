1a88cd4fa9f6be225e17c893325d369b6f8e8e50
Sergey <mazilanx@gmail.com>
2019-12-18 14:10:48 +0300

    added new line chart,       test data wallet,       style for mobile

--- src/app/main/account-page/account-page.component.ts
+++ src/app/main/account-page/account-page.component.ts
@@ -1,14 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AppComponent} from '../../app.component';
-import {results} from "../../../assets/test";
-import {Color} from "ng2-charts";
-import {BorderWidth} from "chart.js";
+import {testData} from "../../../assets/testData";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
-export interface Data {
-  month: String;
-  price: Number;
-}
+am4core.useTheme(am4themes_animated);
 
 @Component({
   selector: 'app-convert-page',
@@ -16,266 +14,79 @@ export interface Data {
   styleUrls: ['./account-page.component.scss']
 })
 
-export class AccountPageComponent implements OnInit {
+export class AccountPageComponent implements OnInit, AfterViewInit, OnDestroy {
+  private chart: am4charts.XYChart;
+  dataWallet = testData;
 
   constructor(
     private readonly http: HttpClient,
     public readonly appComponent: AppComponent,
+    private zone: NgZone
   ) {}
 
-  public doughnutChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
-  public doughnutChartData = [120, 150, 180, 90];
+  public doughnutChartLabels = [];
+  public doughnutChartData = [];
   public doughnutChartType = 'doughnut';
-
-
-  // public ChartLabels = [
-  //   'January',
-  //   'February',
-  //   'March',
-  //   'April',
-  //   'May',
-  //   'June',
-  //   'July',
-  //   'August',
-  //   'September',
-  //   'October',
-  //   'November',
-  //   'December'
-  // ];
-  public ChartLabels = [
-    "March 12, 2018"
-    ,"March 13, 2018	"
-    ,"March 14, 2018	"
-    ," March 15, 2018"
-    ," March 16, 2018"
-    ," March 17, 2018"
-    ," March 18, 2018"
-    ," March 19, 2018"
-    ," March 20, 2018"
-    ," March 21, 2018"
-    ," March 22, 2018"
-    ," March 23, 2018"
-    ," March 24, 2018"
-    ," March 25, 2018"
-    ," March 26, 2018"
-    ," March 27, 2018"
-    ," March 28, 2018"
-    ," March 29, 2018"
-    ," March 30, 2018"
-    ," March 31, 2018"
-    ," April 01, 2018"
-    ," April 02, 2018"
-    ," April 03, 2018"
-    ," April 04, 2018"
-    ," April 05, 2018"
-    ," April 06, 2018"
-    ," April 07, 2018"
-    ," April 08, 2018"
-    ," April 09, 2018"
-    ," April 10, 2018"
-    ," April 11, 2018"
-    ," April 12, 2018"
-    ," April 13, 2018"
-    ," April 14, 2018"
-    ," April 15, 2018"
-    ," April 16, 2018"
-    ," April 17, 2018"
-    ," April 18, 2018"
-    ," April 19, 2018"
-    ," April 20, 2018"
-    ," April 21, 2018"
-    ," April 22, 2018"
-    ," April 23, 2018"
-    ," April 24, 2018"
-    ," April 25, 2018"
-    ," April 26, 2018"
-    ," April 27, 2018"
-    ," April 28, 2018"
-    ," April 29, 2018"
-    ," April 30, 2018"
-    ," May 01, 2018"
-    ," May 02, 2018"
-    ," May 03, 2018"
-    ," May 04, 2018"
-    ," May 05, 2018"
-    ," May 06, 2018"
-    ," May 07, 2018"
-    ," May 08, 2018"
-    ," May 09, 2018"
-    ," May 11, 2018"
-    ," May 12, 2018"
-    ," May 13, 2018"
-    ," May 14, 2018"
-    ," May 15, 2018"
-    ," May 16, 2018"
-    ," May 17, 2018"
-    ," May 18, 2018"
-    ," May 19, 2018"
-    ," May 20, 2018"
-    ," May 21, 2018"
-    ," May 22, 2018"
-    ," May 23, 2018"
-    ," May 24, 2018"
-    ," May 25, 2018"
-    ," May 26, 2018"
-    ," May 27, 2018"
-    ," May 28, 2018"
-    ," May 29, 2018"
-    ," May 30, 2018"
-    ," May 31, 2018"
-    ," June 01, 2018"
-    ," June 02, 2018"
-    ," June 03, 2018"
-    ," June 04, 2018"
-    ," June 05, 2018"
-    ," June 06, 2018"
-    ," June 07, 2018"
-    ," June 08, 2018"
-    ," June 09, 2018"
-    ," June 10, 2018"
-    ," June 11, 2018"
-    ," June 12, 2018"
-  ];
-  public ChartData = [
-    3418
-    ,2673
-    ,49
-    ,2303
-    ,1204
-    ,3009
-    ,4114
-    ,3041
-    ,2370
-    ,2136
-    ,1039
-    ,2030
-    ,2729
-    ,3759
-    ,2584
-    ,2060
-    ,2071
-    ,2039
-    ,1990
-    ,2418
-    ,805
-    ,123
-    ,1781
-    ,1776
-    ,2000
-    ,2122
-    ,2091
-    ,3402
-    ,2274
-    ,1768
-    ,1441
-    ,1850
-    ,1776
-    ,1994
-    ,2978
-    ,2109
-    ,1320
-    ,1804
-    ,1842
-    ,1664
-    ,2043
-    ,2827
-    ,2000
-    ,1652
-    ,1709
-    ,1309
-    ,1746
-    ,1628
-    ,2645
-    ,2010
-    ,1492
-    ,1556
-    ,1617
-    ,1499
-    ,1665
-    ,2073
-    ,1857
-    ,1503
-    ,1524
-    ,1490
-    ,1373
-    ,1998
-    ,2596
-    ,1750
-    ,1486
-    ,1317
-    ,1435
-    ,1538
-    ,1883
-    ,1766
-    ,1546
-    ,1303
-    ,1294
-    ,1313
-    ,1366
-    ,1081
-    ,1351
-    ,1191
-    ,1482
-    ,874
-    ,894
-    ,1296
-    ,1569
-    ,2098
-    ,1643
-    ,1331
-    ,1375
-    ,1393
-    ,1384
-    ,1514
-    ,2075
-    ,1567
-    ,1387
-  ];
-  public ChartType = 'line';
-  lineChartColors: Color[] = [
-    {
-      borderColor: '#0f59d1',
-      backgroundColor: '#0f59d11f',
-    },
-  ];
-  // lineChartBorderWidth: BorderWidth[] = [
-  //   {
-  //     borderWidth: 2
-  //   },
-  // ];
   public doughnutChartOptions: any = {
-
     legend: {
       display: false,
     },
-    responsive: true
-  };
-
-  public barChartOptions: any = {
-
-    legend: {
-      display: false,
-    },
-    animation: {
-      animateScale: true,
-      animateRotate: true
-    },
-    scales: {
-      xAxes: [{
-        type: 'time',
-        time: {
-          unit: 'month'
-        }
-      }]
-    },
-    scaleShowVerticalLines: false,
     responsive: true
   };
 
   ngOnInit() {
+    this.dataWallet.map((item) => {
+      this.doughnutChartData.push(item.value);
+      this.doughnutChartLabels.push(item.code);
+    })
   }
 
-  updateChart() {
+  ngAfterViewInit() {
+    this.drawingChart();
+  }
 
+  ngOnDestroy() {
+    this.zone.runOutsideAngular(() => {
+      if (this.chart) {
+        this.chart.dispose();
+      }
+    });
+  }
+
+  drawingChart() {
+    this.zone.runOutsideAngular(() => {
+      let chart = am4core.create("chart", am4charts.XYChart);
+
+      chart.paddingRight = 20;
+
+      let data = [];
+      let visits = 10;
+      for (let i = 1; i < 366; i++) {
+        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+        data.push({ date: new Date(2019, 0, i), name: "name" + i, value: visits });
+      }
+
+      chart.data = data;
+
+      let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+      dateAxis.renderer.grid.template.location = 0;
+
+      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.tooltip.disabled = true;
+      valueAxis.renderer.minWidth = 35;
+
+      let series = chart.series.push(new am4charts.LineSeries());
+      series.dataFields.dateX = "date";
+      series.dataFields.valueY = "value";
+
+      series.tooltipText = "{valueY.value}";
+      chart.cursor = new am4charts.XYCursor();
+
+      let scrollbarX = new am4charts.XYChartScrollbar();
+      scrollbarX.series.push(series);
+      chart.scrollbarX = scrollbarX;
+
+      this.chart = chart;
+    });
   }
 }
---
+++ src/assets/testData.ts
@@ -0,0 +1,32 @@
+export const testData = [
+  {
+    'code': 'XDR',
+    'name': 'Pays XDR',
+    'icon': '/assets/img/xdr-coin.svg',
+    'balance': 0.01,
+    'percent': '30',
+    'value': '97',
+    'change': '+5%',
+    'chart': [0, 1, 2, 1, 0, 4, 7, 3, 1, 2, 1, 0, 4, 7, 3],
+  },
+  {
+    'code': 'XLM',
+    'name': 'Lumen',
+    'icon': '/assets/img/XLM.png',
+    'balance': 100,
+    'percent': '33',
+    'value': '100',
+    'change': '-',
+    'chart': [0, 1, 2, 1, 0, 4, 7, 3, 1, 2, 1, 0, 4, 7, 3],
+  },
+  {
+    'code': 'BTC',
+    'name': 'Bitcoin',
+    'icon': 'https://apay.io/public/logo/btc.svg',
+    'balance': 1000,
+    'percent': '37',
+    'value': '109',
+    'change': '-2%',
+    'chart': [0, 1, 2, 1, 0, 4, 7, 3, 1, 2, 1, 0, 4, 7, 3],
+  }
+];
