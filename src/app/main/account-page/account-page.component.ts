c9bc684f571ca135cb07666e15879bc7bb8222ee
Sergey <mazilanx@gmail.com>
2019-12-20 17:23:31 +0300

    added chart line from chart.js       test data

--- src/app/main/account-page/account-page.component.ts
+++ src/app/main/account-page/account-page.component.ts
@@ -1,12 +1,14 @@
-import {AfterViewInit, Component, NgZone, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AppComponent} from '../../app.component';
 import {testData} from "../../../assets/testData";
-import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import {Color} from "ng2-charts";
+import * as moment from 'moment';
 
-am4core.useTheme(am4themes_animated);
+export interface Data {
+  date: string;
+  val: string;
+}
 
 @Component({
   selector: 'app-convert-page',
@@ -14,79 +16,95 @@ am4core.useTheme(am4themes_animated);
   styleUrls: ['./account-page.component.scss']
 })
 
-export class AccountPageComponent implements OnInit, AfterViewInit, OnDestroy {
-  private chart: am4charts.XYChart;
+export class AccountPageComponent implements OnInit {
   dataWallet = testData;
+  public doughnutChartLabels = [];
+  public doughnutChartData = [];
+  public doughnutChartType = 'doughnut';
 
   constructor(
     private readonly http: HttpClient,
     public readonly appComponent: AppComponent,
-    private zone: NgZone
   ) {}
 
-  public doughnutChartLabels = [];
-  public doughnutChartData = [];
-  public doughnutChartType = 'doughnut';
+  public ChartLabels = [];
+  public ChartData = [];
+  public ChartType = 'line';
+  lineChartColors: Color[] = [
+    {
+      borderColor: '#0f59d1',
+      backgroundColor: '#0f59d11f',
+    },
+  ];
+
   public doughnutChartOptions: any = {
+
     legend: {
       display: false,
     },
     responsive: true
   };
 
+  public barChartOptions: any = {
+
+    legend: {
+      display: false,
+    },
+    animation: {
+      animateScale: true,
+      animateRotate: true
+    },
+    scaleShowVerticalLines: false,
+    responsive: true,
+    scaleOverride: true,
+    scales: {
+      xAxes: [{
+        ticks: {
+          fontColor: "#000",
+          fixedSize: 3,
+        },
+        type: "time",
+        time: {
+          // unit: "month"
+          tooltipFormat: "ll"
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          fontColor: "#000",
+          fixedSize: 1,
+        }
+      }]
+    },
+    tooltips: {
+      position: "nearest",
+      mode: "index",
+      intersect: false,
+    },
+  };
+
   ngOnInit() {
+    this.drawingChart('AED', 30,"days")
     this.dataWallet.map((item) => {
       this.doughnutChartData.push(item.value);
       this.doughnutChartLabels.push(item.code);
-    })
-  }
-
-  ngAfterViewInit() {
-    this.drawingChart();
-  }
-
-  ngOnDestroy() {
-    this.zone.runOutsideAngular(() => {
-      if (this.chart) {
-        this.chart.dispose();
-      }
     });
   }
 
-  drawingChart() {
-    this.zone.runOutsideAngular(() => {
-      let chart = am4core.create("chart", am4charts.XYChart);
+  async drawingChart(select_val, time_amount,time_type) {
+      const nowtime = moment().format("YYYY-MM-DD");
+      const time = moment().subtract(time_amount,  time_type).format("YYYY-MM-DD");
+      await this.updateChart(select_val, time, nowtime);
+  }
 
-      chart.paddingRight = 20;
-
-      let data = [];
-      let visits = 10;
-      for (let i = 1; i < 366; i++) {
-        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-        data.push({ date: new Date(2019, 0, i), name: "name" + i, value: visits });
-      }
-
-      chart.data = data;
-
-      let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-      dateAxis.renderer.grid.template.location = 0;
-
-      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-      valueAxis.tooltip.disabled = true;
-      valueAxis.renderer.minWidth = 35;
-
-      let series = chart.series.push(new am4charts.LineSeries());
-      series.dataFields.dateX = "date";
-      series.dataFields.valueY = "value";
-
-      series.tooltipText = "{valueY.value}";
-      chart.cursor = new am4charts.XYCursor();
-
-      let scrollbarX = new am4charts.XYChartScrollbar();
-      scrollbarX.series.push(series);
-      chart.scrollbarX = scrollbarX;
-
-      this.chart = chart;
+  async updateChart (select_val, time, nowtime) {
+    this.ChartLabels = [];
+    this.ChartData = [];
+    return this.http.get(`https://back.paysxdr.com/ratesHistory?start=${time}&finish=${nowtime}&base=${select_val}&currency=XDR`).subscribe((data: [Data]) => {
+      data.map(item => {
+        this.ChartLabels.push(item.date);
+        this.ChartData.push(item.val);
+      })
     });
   }
 }
