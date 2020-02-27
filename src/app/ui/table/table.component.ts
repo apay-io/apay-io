780a8d5cac491bdc08942064cde2059be4675a3e
MazilaNx <Mazilanx@gmail.com>
2020-02-28 02:45:56 +0300

    add the translation of the table in euros or xdr (#53)
    fixed #43

--- src/app/ui/table/table.component.ts
+++ src/app/ui/table/table.component.ts
@@ -4,15 +4,16 @@ import {MatTableDataSource} from '@angular/material/table';
 import {sparkline} from '@fnando/sparkline';
 import {CurrencySelectionService} from '../../core/currency-selection.service';
 import {GetCurrenciesServices} from '../../core/get-currencies.services';
+import {HttpClient} from '@angular/common/http';
 
 export interface UserData {
-    icon: string;
-    name: string;
-    change: string;
-    price: string;
-    volume: string;
-    depthUsd: string;
-    priceUsd;
+  icon: string;
+  name: string;
+  change: string;
+  price: string;
+  volume: string;
+  depthUsd: string;
+  priceUsd;
 }
 
 @Component({
@@ -22,55 +23,70 @@ export interface UserData {
 })
 
 export class TableComponent implements OnInit, OnDestroy {
-    displayedColumns: string[] = ['icon', 'name', 'change', 'usd', 'volume', 'depth', 'graph', 'convert'];
-    dataSource: MatTableDataSource<UserData>;
+  displayedColumns: string[] = ['icon', 'name', 'change', 'usd', 'volume', 'depth', 'graph', 'convert'];
+  dataSource: MatTableDataSource<UserData>;
 
-    @Input()
-    stats$;
+  @Input()
+  stats$;
 
-    @ViewChild(MatSort, {static: true}) sort: MatSort;
-    currencies;
-    getCurrenciesSub;
-    initSparklineTimeOut;
-    selected = 'USD';
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  currencies;
+  getCurrenciesSub;
+  initSparklineTimeOut;
+  rates;
+  selected = 'USD';
 
-    constructor(public currencySelectionService: CurrencySelectionService,
-                private getCurrencies: GetCurrenciesServices,) {
-    }
+  constructor(private readonly http: HttpClient,
+              public currencySelectionService: CurrencySelectionService,
+              private getCurrencies: GetCurrenciesServices,) {
+  }
 
-    ngOnInit() {
-        this.getCurrenciesSub = this.getCurrencies.state$.subscribe((data: any) => {
-            if (data.length) {
-                this.currencies = data;
-                this.dataSource = new MatTableDataSource(this.currencies);
-                this.dataSource.sort = this.sort;
-                this.dataSource.connect().subscribe((data: any) => {
-                    clearTimeout(this.initSparklineTimeOut);
-                    if (window.innerWidth >= 1024 && data.length > 0) {
-                        this.initSparklineTimeOut = setTimeout(() => {
-                            data.forEach((item, id) => {
-                                const min = Math.min(...item.priceUsd);
-                                const adjustedPrices = item.priceUsd.map((price) => price - min);
-                                sparkline(document.getElementById(`sparkline-${item.code}`), adjustedPrices);
-                            });
-                        }, 200);
-                    }
-                });
-            }
-        })
-    }
-
-    ngOnDestroy() {
-      if (this.getCurrenciesSub) {
-        this.getCurrenciesSub.unsubscribe();
+  ngOnInit() {
+    this.http.get(`https://rates.apay.io`).subscribe((data) => {
+      this.rates = data;
+    });
+    this.getCurrenciesSub = this.getCurrencies.state$.subscribe((data: any) => {
+      if (data.length) {
+        console.log(data);
+        this.currencies = data;
+        this.dataSource = new MatTableDataSource(this.currencies);
+        this.dataSource.sort = this.sort;
+        this.dataSource.connect().subscribe((data: any) => {
+          clearTimeout(this.initSparklineTimeOut);
+          if (window.innerWidth >= 1024 && data.length > 0) {
+            this.initSparklineTimeOut = setTimeout(() => {
+              data.forEach((item, id) => {
+                const min = Math.min(...item.priceUsd);
+                const adjustedPrices = item.priceUsd.map((price) => price - min);
+                sparkline(document.getElementById(`sparkline-${item.code}`), adjustedPrices);
+              });
+            }, 200);
+          }
+        });
       }
-    }
+    });
+  }
 
-    applyFilter(filterValue: string) {
-        this.dataSource.filter = filterValue.trim().toLowerCase();
+  ngOnDestroy() {
+    if (this.getCurrenciesSub) {
+      this.getCurrenciesSub.unsubscribe();
     }
+  }
 
-    currencySelection(event, type: 'buy' | 'sell') {
-        this.currencySelectionService.changeCurrency(event, type);
-    }
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  currencySelection(event, type: 'buy' | 'sell') {
+    this.currencySelectionService.changeCurrency(event, type);
+  }
+
+  selectUnit() {
+    this.currencies.map((item) => {
+      item.price = (item.price / this.rates[item.currentUnit]) * this.rates[this.selected];
+      item.volume = (item.volume / this.rates[item.currentUnit]) * this.rates[this.selected];
+      item.depthUsd = (item.depthUsd / this.rates[item.currentUnit]) * this.rates[this.selected];
+      item.currentUnit = this.selected;
+    });
+  }
 }
